2c92912775047aaf03f21c30a3d4b4a0e5a65083
Giovani Marchezini <97356190+giovanimarchezini@users.noreply.github.com>
2025-01-10 01:23:28 +0000

    add arquivos database.js e .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import databese from '../../../../infra/database.js'
+
+async function status(request, response) {
+  const result = await databese.query("SELECT 1 + 1;")
+  console.log(result)
   response.status(200).json({ message: "teste" })
 }
 
